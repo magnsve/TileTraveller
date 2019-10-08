ffdbc3181e759112d73fb3d1effa2753d1477abf
Magnús Sveinsson <magnus10@ru.is>
2019-10-08 17:32:33 +0000

    dæmatími 8.10

--- tile_traveller.py
+++ tile_traveller.py
@@ -6,43 +6,42 @@
 # We start by defining all cells and their properties.
 # Next we create a function that asks for user input and validates it.
 
-def cell(x,y):    
-    ''' This function defines each cell and it's properties '''
+NORTH = 'n'
+SOUTH = 's'
+WEST = 'w'
+EAST = 'e'
 
-    if x == 1 and y == 1:
-        direction = 'nN' 
-        print('You can travel: (N)orth.')
-    elif x == 1 and y == 2:
-        direction = 'nseNSE'
-        print('You can travel: (N)orth or (E)ast or (S)outh.')
-    elif x == 1 and y == 3:
-        direction = 'esES'
-        print('You can travel: (E)ast or (S)outh.')
-    elif x == 2 and y == 3:
-        direction = 'ewEW'
-        print('You can travel: (E)ast or (W)est.')
-    elif x == 3 and y == 3:
-        direction = 'wsWS'
-        print('You can travel: (S)outh or (W)est.')
-    elif x == 3 and y == 2:
-        direction = 'nsNS'
-        print('You can travel: (N)orth or (S)outh.')
-    elif x == 3 and y == 1:
-        direction = 'nN'
-        print('Victory!')
-    elif x == 2 and y == 2:
-        direction = 'wsWS'
-        print('You can travel: (W)est or (S)outh.')
-    elif x == 2 and y == 1:
-        direction = 'nN'
-        print('You can travel: (N)orth.')
+def print_directions(direction_string):
+    print("You can travel: ", end='')
+    first = True
+    for char in direction_string:
+        if not first:
+            print(" or ", end='')
+        if char == NORTH:
+            print("(N)orth",end = '')
+        if char == EAST:
+            print("(E)ast", end = '')
+        if char == SOUTH:
+            print("(S)outh", end= '')
+        if char == WEST:
+            print("(W)est", end = '')
+        first = False
+    print('.')
 
-    return direction
 
-def is_valid(user_input,definition):
+def cell(coordinates):    
+    ''' This function defines each cell and it's properties '''    
+    cells = [[(1,1),NORTH], [(1,2),NORTH+SOUTH+EAST], [(1,3),EAST+SOUTH], [(2,3),EAST+WEST], [(3,3),WEST+SOUTH], [(3,2),NORTH+SOUTH], [(3,1),NORTH], [(2,2),WEST+SOUTH], [(2,1),NORTH]]
+    for element in cells:
+        element_coordinates = element[0]
+        if element_coordinates == coordinates:
+            directions = element[1]
+    return directions
+
+def is_valid(user_input,directions):
     '''Validation for user input'''
 
-    for ch in definition:
+    for ch in directions:
         if ch == user_input:
             result = True
             break
@@ -54,33 +53,34 @@ def is_valid(user_input,definition):
 
 def travel(cellvalue):
     ''' Prints out current positon, gets user input and updates cell values '''
-    x = int(cellvalue/10)
-    y = cellvalue%10
-    possible_directions = cell(x,y)
+    x, y = cellvalue
+    possible_directions = cell(cellvalue)
+    print_directions(possible_directions)
     legal_move = False
 
     while legal_move == False:
         dir = input('Direction: ')
+        dir = dir.lower()
         legal_move = is_valid(dir,possible_directions) 
 
-    if dir == 'n' or dir =='N':
+    if dir == 'n':
         y += 1
-    elif dir == 's' or dir == 'S':
+    elif dir == 's':
         y -= 1
-    elif dir == 'e' or dir == 'E':
+    elif dir == 'e':
         x += 1
-    elif dir == 'w' or dir == 'W':
+    elif dir == 'w':
         x-= 1
 
-    cellvalue = int(str(x)+str(y))
+    cellvalue = (x,y)
 
     return cellvalue
 
 
 # Now we can play the game. Start by setting start position to 1,1.
-cellvalue = 11
+cellvalue = (1,1)
 
-while cellvalue != 31:
+while cellvalue != (3,1):
     cellvalue = travel(cellvalue)
 else: 
     print('Victory!')
